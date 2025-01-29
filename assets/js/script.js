40b703f758e9fb78590c88bac7bd49b87f433584
Mina <TMDHammond@outlook.com>
2025-01-28 18:37:35 -0800

    Added Light/Dark button and updated CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ function toggleImage(card) {
     const img = card.querySelector('img');
     const text = card.querySelector('p');
     
+    
     if (img.style.display !== 'none') {
       img.style.display = 'none'; // Hide the image
       text.style.display = 'block'; // Show the text
